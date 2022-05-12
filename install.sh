bd5f6e48cd695ea9ae79570ddb5e40a41f7a593a
Bushaev Denis <32451886+Dez64ru@users.noreply.github.com>
2022-05-12 16:20:36 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-echo ‘install brew’
+echo install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo ‘install core’
+echo install core
 brew install git nvm
 
-nvmpath=<<EOT
-export NVM_DIR="$HOME/.nvm"
-. "/opt/homebrew/opt/nvm/nvm.sh"
-EOT
+nvmpath="export NVM_DIR=\"$HOME/.nvm\"\n. \"/opt/homebrew/opt/nvm/nvm.sh\""
 
-[ -d "~/.nvm" ] && mkdir ~/.nvm
+[ -d "$HOME/.nvm" ] && mkdir ~/.nvm
 
 if ! grep -q nvm ~/.zshrc; then
     echo "Install nvm path into /.zshrc"
@@ -23,26 +20,26 @@ if ! grep -q nvm ~/.bash_profile; then
     echo "$nvmpath" > ~/.bash_profile
 fi
 
-echo ‘install browsers’
+echo install browsers
 brew install --cask google-chrome tor-browser
 
-echo ‘install devtools’
+echo install devtools
 brew install --cask visual-studio-code postman phpstorm docker valentina-studio termius
 
-echo ‘install communication’ brew install --cask asana
+echo install communication brew install --cask asana
 brew install --cask linear-linear slack telegram
 
-echo ‘install os extends’
+echo install os extends
 brew install --cask raycast hiddenbar displaperture rectangle maccy
 
-echo ‘install editors’
+echo install editors
 brew install --cask figma notion
 
-echo ‘instal media’
+echo instal media
 brew install --cask vlc
 
-echo ‘install network’
+echo install network
 brew install --cask lulu
 
-echo ‘install other’
+echo install other
 brew install --cask qbittorrent the-unarchiver
