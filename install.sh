83b5f25df91e633627dba1cfa92ec6f23081acda
Bushaev Denis <32451886+dez64ru@users.noreply.github.com>
2022-05-15 23:18:44 +0400

    minor fixes

--- install.sh
+++ install.sh
@@ -1,70 +1,65 @@
 #!/bin/sh
 
-echo install brew
+echo 'install brew'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo install core
+echo 'install core'
 brew install git nvm
 npm install -g npkill
 
+echo 'init nvm'
 nvmpath="export NVM_DIR=\"$HOME/.nvm\"\n. \"/opt/homebrew/opt/nvm/nvm.sh\""
-
 [ -d "$HOME/.nvm" ] && mkdir ~/.nvm
+[ ! grep -q nvm ~/.zshrc ] && { echo "Install nvm path into /.zshrc"; echo "$nvmpath" > ~/.zshrc; }
+[ ! grep -q nvm ~/.bash_profile ] && { echo "Install nvm path into /.bash_profile"; echo "$nvmpath" > ~/.bash_profile; }
 
-if ! grep -q nvm ~/.zshrc; then
-    echo "Install nvm path into /.zshrc"
-    echo "$nvmpath" > ~/.zshrc
-fi
-
-if ! grep -q nvm ~/.bash_profile; then
-    echo "Install nvm path into /.bash_profile"
-    echo "$nvmpath" > ~/.bash_profile
-fi
-
-echo install browsers
+echo 'install browsers'
 brew install --cask google-chrome tor-browser
 
-echo install devtools
+echo 'install devtools'
 brew install --cask visual-studio-code postman phpstorm docker valentina-studio termius
 
-echo install communication brew install --cask asana
+echo 'install communication'
+brew install --cask asana
 brew install --cask linear-linear slack telegram
 
-echo install os extends
+echo 'install os extends'
 brew install --cask raycast hiddenbar displaperture rectangle maccy
 
-echo install editors
+echo 'install editors'
 brew install --cask figma notion
 
-echo instal media
+echo 'instal media'
 brew install --cask vlc
 
-echo install network
+echo 'install network'
 brew install --cask lulu
 
-echo install other
+echo 'install other'
 brew install --cask qbittorrent the-unarchiver
 
-echo install fonts
+echo 'install fonts'
 mkdir ~/.tmp && cd ~/.tmp
 curl -s -L https://github.com/kube/sf-mono-ligaturized/archive/refs/heads/master.zip -o fonts && unzip fonts
 mv sf-mono-ligaturized-master/ligaturized/* ~/Library/Fonts/
 rm -rf ~/.tmp && cd ~
 
-echo disable indexing
+echo 'disable indexing'
 sudo mdutil -i off /
 sudo mdutil -E /
 
-echo enable show all files
+echo "\n"
+echo 'MacOs Tweaks'
+echo 'enable show all files'
 defaults write http://com.apple.Finder AppleShowAllFiles true
-echo disable open this application
+echo 'disable open this application'
 defaults write com.apple.LaunchServices LSQuarantine -bool false
-echo enable snap to grid desktop
+echo 'enable snap to grid desktop'
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-echo disable write .DS_store
+echo 'disable write .DS_store'
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-echo brew uucd
+echo 'Init crontab task for brew full update'
 sudo crontab -l > cron_bkp
 sudo echo "0 10 * * * brew update && brew upgrade && brew cleanup; brew doctor >/dev/null 2>&1" >> cron_bkp
 sudo crontab cron_bkp
